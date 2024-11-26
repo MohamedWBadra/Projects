3807ea918c889630e01eee7cd7c4c301a62dc0c5
MohamedWBadra <167582456+MohamedWBadra@users.noreply.github.com>
2024-11-27 00:39:55 +0200

    Update Supplier.java

--- Codes/Supplier.java
+++ Codes/Supplier.java
@@ -2,8 +2,33 @@ import java.util.ArrayList;
 public class Supplier implements Viewable {
     String name;
     ArrayList<Products> myProducts;
-    Supplier(String n){}
-    public void addproduct(/*hanshof hanb3at eh ba3den */){}
-    public void viewbycategory(String cat){}
-    public void viewallproducts(){}  
+    Supplier(String n)
+    {
+        this.name=n;
+        myProducts=new ArrayList<Products>();
+    }
+    public void addproduct(Products p)
+    {
+            if(!myProducts.contains(p))
+            {
+               myProducts.add(p); 
+
+            }
+        
+    }
+    public void viewByCategory(String cat){
+       for(int i=0;i<myProducts.size();i++)
+       {
+        if(myProducts.get(i).getType()==cat)
+        {
+            System.out.println(myProducts.get(i)+" ");
+        }
+       }
+    }
+    public void viewAllProducts(){
+        for(int i=0;i<myProducts.size();i++)
+        {
+            System.out.println("Name: "+myProducts.get(i).getName()+" price: "+myProducts.get(i).getPrice());
+        }
+}
 }
