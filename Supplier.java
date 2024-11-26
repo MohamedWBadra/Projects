caf68750a703330accd621b557da159f751a08be
MohamedWBadra <167582456+MohamedWBadra@users.noreply.github.com>
2024-11-26 22:19:56 +0200

    Delete Supplier.java

--- Supplier.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-public class Supplier implements Viewable {
-    String name;
-    ArrayList<Products> myProducts;
-    Supplier(String n){}
-    public void addproduct(/*hanshof hanb3at eh ba3den */){}
-    public void viewbycategory(String cat){}
-    public void viewallproducts(){}  
-}
