67a3076be81ee5c325416e66d0c6ed29c24b720e
Moaz715 <moazfathy592005@gmail.com>
2024-11-27 02:10:43 +0200

    Update Database.java

--- code2.0/Database.java
+++ code2.0/Database.java
@@ -4,4 +4,5 @@ public class Database {
     public static ArrayList<Supplier> suppliers = new ArrayList<Supplier>();
     public static ArrayList<Customer> Customers = new ArrayList<Customer>();
     public static ArrayList<Admin> Admins = new ArrayList<Admin>();
+    public static ArrayList<Order> Orders = new ArrayList<Order>();
 }
