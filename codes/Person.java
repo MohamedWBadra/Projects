683892f83fd7b39536f17a50a98d29b44f809e5d
MohamedElHadary05 <MrNoob000000@gmail.com>
2024-11-28 19:54:34 +0200

    Add files via upload

---
+++ codes/Person.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+abstract public class Person {
+    protected String username;
+    protected String password;
+    protected Date date_of_birth;
+
+    Person(String username, String password,int year,int months,int days){
+        this.username = username;
+        this.password = password;
+        date_of_birth = new Date(year,months, days);
+    }
+    public String getUsername() {
+        return username;
+    }
+
+
+}
