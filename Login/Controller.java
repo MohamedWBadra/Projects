84a9efd10c0baa0ba8af1e372c16b87a3bdcc119
omarrrfouad <omarfouadomar33@gmail.com>
2024-12-15 21:11:35 +0200

    Add files via upload

---
+++ Login/Controller.java
@@ -0,0 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private PasswordField tfPassword;
+
+    @FXML
+    private TextField tfUserName;
+
+    @FXML
+    void onBtnClick(ActionEvent event) {
+
+    }
+
+}
